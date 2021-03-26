42df853593c0231f9b6e80acf607842c2d1be612
Zoya-Hashmi <zoyahashmi17@yahoo.com>
2021-03-26 18:40:20 +0000

    fixing download.sh

--- utils/download_data.sh
+++ utils/download_data.sh
@@ -1,15 +1,15 @@
 # Download everything
-wget --show-progress -O data/attr-ops-data.tar.gz https://www.cs.utexas.edu/~tushar/attribute-ops/attr-ops-data.tar.gz
+# wget --show-progress -O data/attr-ops-data.tar.gz https://www.cs.utexas.edu/~tushar/attribute-ops/attr-ops-data.tar.gz
 # wget --show-progress -O data/mitstates.zip http://wednesday.csail.mit.edu/joseph_result/state_and_transformation/release_dataset.zip
 wget --show-progress -O data/utzap.zip http://vision.cs.utexas.edu/projects/finegrained/utzap50k/ut-zap50k-images.zip
-wget --show-progress -O tensor-completion/bptf.tar.bz https://www.cs.cmu.edu/~lxiong/bptf/export_bptf.tar.bz
+# wget --show-progress -O tensor-completion/bptf.tar.bz https://www.cs.cmu.edu/~lxiong/bptf/export_bptf.tar.bz
 echo "Data downloaded. Extracting files..."
 
 # Dataset metadata, pretrained SVMs and features, tensor completion data
-tar -zxvf data/attr-ops-data.tar.gz --strip 1
+# tar -zxvf data/attr-ops-data.tar.gz --strip 1
 
 # tensor completion code
-tar -xvf tensor-completion/bptf.tar.bz -C tensor-completion/ --strip 1
+# tar -xvf tensor-completion/bptf.tar.bz -C tensor-completion/ --strip 1
 
 # dataset images
 cd data/
@@ -21,12 +21,14 @@ cd data/
 # rename "s/ /_/g" mit-states/images/*
 
 # UT-Zappos50k
-unzip utzap.zip -d ut-zap50k/
-mv ut-zap50k/ut-zap50k-images ut-zap50k/_images/
+unzip utzap.zip -d .
+
+mv ut-zap50k-images ut-zap50k/_images/
 
 cd ..
 python utils/reorganize_utzap.py
 
 # remove all zip files and temporary files
 # rm -r data/attr-ops-data.tar.gz data/mitstates.zip data/utzap.zip data/ut-zap50k/_images
-rm -r data/attr-ops-data.tar.gz data/utzap.zip data/ut-zap50k/_images
+rm -r data/utzap.zip data/ut-zap50k/_images
+# rm -r data/compositional-split data/metadata.t7
